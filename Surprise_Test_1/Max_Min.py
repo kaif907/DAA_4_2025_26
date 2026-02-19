e28434a09b051aa7a03b0c96dd3a49af781dc69d
Kaific Maliqon <iamkaif368@gmail.com>
2026-02-19 14:39:58 +0530

    Implement maxMin function to calculate unfairness

---
+++ Surprise_Test_1/Max_Min.py
@@ -0,0 +1,9 @@
+def maxMin(k, arr):
+    arr.sort()
+    min_unfairness = float('inf')
+    
+    for i in range(len(arr) - k + 1):
+        unfairness = arr[i + k - 1] - arr[i]
+        min_unfairness = min(min_unfairness, unfairness)
+        
+    return min_unfairness
